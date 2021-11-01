74bacf2afb2513889a8dd19f7f7eab6536c61ba9
jecndlria <jecndlria@gmail.com>
2021-11-01 14:23:38 -0700

    whats 9 + 10

---
+++ addtest.cpp
@@ -0,0 +1,17 @@
+#ifndef __ADD_TEST_HPP__
+#define __ADD_TEST_HPP__
+
+#include "gtest/gtest.h"
+
+#include "add.hpp"
+#include "op.hpp"
+TEST(AddTest, WhatsNinePlusTen) {
+    Op* test0 = new Op(9);
+    Op* test1 = new Op(10);
+    Add* test2 = new Add(test0, test1);
+    EXPECT_EQ(test2->evaluate(), 19);
+}
+
+
+#endif //__OP_TEST_HPP__
+
