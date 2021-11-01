511ce713cebdbcc6282816c4e7cb10a83105e11e
jecndlria <jecndlria@gmail.com>
2021-11-01 14:30:37 -0700

    lol

--- addtest.cpp
+++ addtest.cpp
@@ -12,6 +12,12 @@ TEST(AddTest, WhatsNinePlusTen) {
     EXPECT_EQ(test2->evaluate(), 19);
 }
 
+TEST(AddTest, WhatsNinePlusTenString) {
+    Op* test0 = new Op(9);
+    Op* test1 = new Op(10);
+    Add* test2 = new Add(test0, test1);
+    EXPECT_EQ(test2->stringify(), "9 + 10");
+}
 
 #endif //__OP_TEST_HPP__
 
